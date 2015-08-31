2292590d6146b5ab39d59c7be0f8eef1eeb89a3f
Igal Tabachnik <hmemcpy@gmail.com>
2015-08-31 17:00:47 +0300

    Adding icon and nuget package generation

--- NServiceBus.Wire/Properties/AssemblyInfo.cs
+++ NServiceBus.Wire/Properties/AssemblyInfo.cs
@@ -1,36 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NServiceBus.Wire")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NServiceBus.Wire")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("29ed7a0c-9a61-4416-a020-aab1e1c5c743")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
+![Icon](https://raw.githubusercontent.com/hmemcpy/NServiceBus.Wire/master/icon.png)
+
 NServiceBus.Wire
 ===========================
 
-Add support for [NServiceBus](http://particular.net/NServiceBus) message serialization via [Wire](https://github.com/rogeralsing/Wire)
+[![NuGet Status](http://img.shields.io/nuget/v/NServiceBus.Wire.svg?style=flat)](https://www.nuget.org/packages/NServiceBus.Wire/)
+
+Add support for [NServiceBus](http://particular.net/NServiceBus) message serialization via [Wire](https://github.com/rogeralsing/Wire) binary serializer.
+
+Icon [Insulated Wire](https://thenounproject.com/term/insulated-wire/116162) (CC BY 3.0) designed by Arthur Shlain from the [Noun Project](http://www.thenounproject.com).
