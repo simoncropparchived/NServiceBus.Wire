fb3831d545d09f898c8e769ed76d2b5740e1099c
Simon Cropp <simon.cropp@gmail.com>
2017-07-12 15:42:38 +1000

    update to net core

--- NServiceBus.Wire/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("NServiceBus.Wire")]
-[assembly: AssemblyProduct("NServiceBus.Wire")]
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1")]
